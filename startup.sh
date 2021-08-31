a52dd1bd761848386877cd91a3c6f9a625878d6a
WilliamOtieno <jimmywilliamotieno@gmail.com>
2021-08-31 15:32:33 +0300

    commit

--- startup.sh
+++ startup.sh
@@ -6,5 +6,10 @@ git clone https://github.com/WilliamOtieno/zsh-config
 cp alacritty-config/alacritty.yml .config/
 cp zsh-config/.zshrc .zshrc
 
+touch "$HOME/.cache/zshhistory"
+#-- Setup Alias in $HOME/zsh/aliasrc
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >> ~/.zshrc
+
 
 sudo pacman -S linux510 linux510-headers -y && reboot
